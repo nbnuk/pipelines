13ba5a97edb62a24e0b5e5a13d1ff5117915a67a
Keith Raven <k.raven@nbn.org.uk>
2024-08-01 16:14:48 +0100

    add migration script suited to emr

---
+++ livingatlas/migration/scripts/migrate-uuids-emr.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+FS_PATH="hdfs:///"
+DATA_PATH="pipelines-data"
+MIGRATION_JAR="/usr/share/la-pipelines/migration.jar"
+
+echo 'Setting up HDFS directories'    
+hdfs dfs -mkdir -p /pipelines-data
+hdfs dfs -chmod 777 /pipelines-data
+
+echo 'Running spark uuid migration job'
+
+/usr/bin/spark-submit \
+--name "Migrate UUIDs" \
+--conf spark.default.parallelism=48 \
+--num-executors 8 \
+--executor-cores 8 \
+--executor-memory 18G \
+--driver-memory 2G \
+--class au.org.ala.pipelines.spark.MigrateUUIDPipeline \
+--driver-java-options "-Dlog4j.configuration=file:/data/la-pipelines/config/log4j.properties" \
+$MIGRATION_JAR \
+--occUuidExportPath=$FS_PATH/migration/occ_uuid.csv \
+--occFirstLoadedExportPath=$FS_PATH/migration/occ_first_loaded_date.csv \
+--targetPath=$FS_PATH/$DATA_PATH \
+
+echo 'List HDFS /$DATA_PATH'
+hdfs dfs -ls /$DATA_PATH
