e6c12cae578893dde3ebbe7abf526f74d857b997
Keith Raven <k.raven@nbn.org.uk>
2025-06-04 11:07:08 +0100

    fix: PR-81 Reverse 386d56f0a617be5b19202328de5a5369c581aa5c "PT-65: added samplingProtocol and raw_taxonID to the index" https://nbnatlas.atlassian.net/browse/PT-81

--- livingatlas/pipelines/src/main/java/au/org/ala/pipelines/transforms/IndexRecordTransform.java
+++ livingatlas/pipelines/src/main/java/au/org/ala/pipelines/transforms/IndexRecordTransform.java
@@ -218,7 +218,7 @@ public class IndexRecordTransform implements Serializable, IndexFields {
     skipKeys.add(DwcTerm.preparations.simpleName());
     skipKeys.add(DwcTerm.datasetID.simpleName());
     skipKeys.add(DwcTerm.datasetName.simpleName());
-    //    skipKeys.add(DwcTerm.samplingProtocol.simpleName());
+    skipKeys.add(DwcTerm.samplingProtocol.simpleName());
     skipKeys.add(DwcTerm.otherCatalogNumbers.simpleName());
     skipKeys.add(DwcTerm.organismQuantity.simpleName());
 
@@ -481,8 +481,6 @@ public class IndexRecordTransform implements Serializable, IndexFields {
 
       // if we already have an interpreted value, prefix with raw_
       if (interpretedFields.contains(key)) {
-        indexRecord.getStrings().put(RAW_PREFIX + key, entry.getValue());
-      } else if (key.equals(DwcTerm.taxonID.simpleName())) {
         indexRecord.getStrings().put(RAW_PREFIX + key, entry.getValue());
       } else {
         if (key.endsWith(DwcTerm.dynamicProperties.simpleName())) {
