4cd91eeaba9edf214aca007efcfce2207614c83b
Keith Raven <k.raven@nbn.org.uk>
2025-09-09 16:38:09 +0100

    spotless apply

--- livingatlas/migration/src/main/java/au/org/ala/pipelines/spark/MigrateUUIDPipeline.java
+++ livingatlas/migration/src/main/java/au/org/ala/pipelines/spark/MigrateUUIDPipeline.java
@@ -126,7 +126,7 @@ public class MigrateUUIDPipeline implements Serializable {
                   row.getString(1) == null || "firstLoaded".equals(row.getString(1)) // skip header
                       ? null
                       : LocalDateTime.parse(row.getString(1), DateTimeFormatter.ISO_DATE_TIME)
-                          .toEpochSecond(ZoneOffset.UTC)
+                              .toEpochSecond(ZoneOffset.UTC)
                           * 1000); // firstLoaded in milliseconds
             },
             Encoders.tuple(Encoders.STRING(), Encoders.LONG()));
